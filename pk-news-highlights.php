ff9d77ac0006468419d15fee6bb5ffd89a8b0fd2
Ashu Tiwary <38155138+ashutiwary@users.noreply.github.com>
2025-06-01 06:30:53 +0000

    add meta box on page for taking summary and link of the particular post

--- pk-news-highlights.php
+++ pk-news-highlights.php
@@ -40,4 +40,38 @@ function pk_register_news_post_type() {
 add_action('init', 'pk_register_news_post_type');
 
 
+// Add Meta Boxes for Summary and Link
+function pk_add_news_meta_boxes() {
+    add_meta_box('pk_news_meta', 'Highlight Details', 'pk_news_meta_box_callback', 'pk_news_highlight', 'normal', 'high');
+}
+
+// Callback function for meta box
+function pk_news_meta_box_callback($post) {
+    $summary = get_post_meta($post->ID, '_pk_summary', true);
+    $link = get_post_meta($post->ID, '_pk_link', true);
+    ?>
+    <p>
+        <label for="pk_summary">Summary:</label><br>
+        <textarea id="pk_summary" name="pk_summary" rows="4" style="width:100%;"><?php echo esc_textarea($summary); ?></textarea>
+    </p>
+    <p>
+        <label for="pk_link">Link:</label><br>
+        <input type="url" id="pk_link" name="pk_link" value="<?php echo esc_url($link); ?>" style="width:100%;" />
+    </p>
+    <?php
+}
+
+function pk_save_news_meta($post_id) {
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+    if (isset($_POST['pk_summary'])) {
+        update_post_meta($post_id, '_pk_summary', sanitize_text_field($_POST['pk_summary']));
+    }
+    if (isset($_POST['pk_link'])) {
+        update_post_meta($post_id, '_pk_link', esc_url_raw($_POST['pk_link']));
+    }
+}
+add_action('add_meta_boxes', 'pk_add_news_meta_boxes');
+add_action('save_post', 'pk_save_news_meta');
+
+
 ?>
