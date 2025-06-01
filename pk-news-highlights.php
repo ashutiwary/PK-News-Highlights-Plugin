297702d913ce9ecebf00a4364f57d22dd37716ea
Ashu Tiwary <38155138+ashutiwary@users.noreply.github.com>
2025-06-01 06:08:20 +0000

    add plugin details and prevent from access directly

---
+++ pk-news-highlights.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: PK News Highlights
+ * Description: Add and display news headlines with summaries and links.
+ * Version: 1.0
+ * Author: Your Name
+ * Text Domain: pk-news-highlights
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+
+?>
